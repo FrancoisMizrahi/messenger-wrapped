11551490756a575b62ed3231d3b1ff8e240ad104
Francois Mizrahi <francois.mizrahi@gmail.com>
2024-02-20 19:50:13 +0100

    Remove temp json file after load

--- utils.py
+++ utils.py
@@ -1,7 +1,11 @@
 import streamlit as st
 import json
 import re
+import os
+
 from functools import partial
+from config import *
+
 
 
 def temp_load(uploaded_file, file_name):
@@ -10,20 +14,25 @@ def temp_load(uploaded_file, file_name):
     with open(file_name, "w") as outfile:
         outfile.write(json_object)
 
+def remove_temp_json(file_name):
+    if os.path.exists(file_name):
+        os.remove(file_name)
+    else:
+        st.warning("The file does not exist", icon="⚠️")
+
 
 def repair_json(file_name):
     escapes = partial(re.compile(rb'\\u00([\da-f]{2})').sub,lambda m: bytes.fromhex(m[1].decode()),)
     with open(file_name, 'rb') as binary_data:
         repaired = escapes(binary_data.read())
     parsed_json = json.loads(repaired)
-    
+    remove_temp_json(file_name)
     return parsed_json
 
 
 def load_json(uploaded_file):
-    file_name = "temp_storage/temp_load.json"
-    temp_load(uploaded_file, file_name)
-    parsed_json= repair_json(file_name)
+    temp_load(uploaded_file, temp_storage_file_name)
+    parsed_json= repair_json(temp_storage_file_name)
     
     return parsed_json
 
